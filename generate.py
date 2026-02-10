e47e2b2ee890768d713e632adf1b5af92c97938d
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-10 18:59:42 -0500

    pushing every step in case computer dies

--- generate.py
+++ generate.py
@@ -12,22 +12,22 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(
-        name="Link0",
+        name="Torso",
         pos=[0, 0, 0.5],
         size=[1, 1, 1]
     )
     pyrosim.Send_Cube(
-        name="Link1",
+        name="BackLeg",
         pos=[0,0,0],
         size=[1, 1, 1]
     )
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,1,1.5])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,1,1.5])
     pyrosim.Send_Cube(
-        name="Link2",
+        name="FrontLeg",
         pos=[0,0,0],
         size=[1, 1, 1]
     )
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,1,-1])
+    pyrosim.Send_Joint( name = "BackLeg_FrontLeg" , parent= "BackLeg" , child = "FrontLeg" , type = "revolute", position = [0,1,-1])
 
     pyrosim.End()
 
