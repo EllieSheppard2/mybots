aa9f3da63908e6d7ca62d0aa15e5cde6f36ff98c
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-10 18:52:59 -0500

    pushing every step in case computer dies

--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
