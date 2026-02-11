ce8d8e58e7c9a90155e03a199a553766c133753c
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-10 20:07:05 -0500

    pushing every step in case computer dies

--- analyze.py
+++ analyze.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
+frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
 
 plt.plot(backLegSensorValues)
+plt.plot(frontLegSensorValues)
+plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -3,7 +3,6 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
 import numpy
-import os
 
 steps_in_sim=100
 
@@ -16,14 +15,18 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(steps_in_sim)
+frontLegSensorValues = numpy.zeros(steps_in_sim)
 print(backLegSensorValues)
 
 for i in range(steps_in_sim):
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     backLegSensorValues[i]=backLegTouch
+    frontLegSensorValues[i]=frontLegTouch
     time.sleep(1/120)
 
 p.disconnect()
 
 numpy.save("data/backLegSensorValues.npy",backLegSensorValues)
+numpy.save("data/frontLegSensorValues.npy",frontLegSensorValues)
