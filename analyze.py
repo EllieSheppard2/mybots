f59a261bd714aac330b39a67737d78846bc1a2da
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-10 20:09:14 -0500

    pushing every step in case computer dies

--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
 
-plt.plot(backLegSensorValues)
-plt.plot(frontLegSensorValues)
+plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
+plt.plot(frontLegSensorValues, label = "Front Leg")
 plt.legend()
 plt.show()
