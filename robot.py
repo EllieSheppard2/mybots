e250fc48ca0b689a25f43612aa11ff07308f81ab
EllieSheppard2 <elliesheppard213@gmail.com>
2026-03-02 19:27:54 -0500

    generate brain

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 from sensor import SENSOR
 from motor import MOTOR
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
     def __init__(self):
@@ -10,6 +11,7 @@ class ROBOT:
         pyrosim.Prepare_To_Simulate(self.robotId)
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
+        self.nn = NEURAL_NETWORK("brain.nndf")
     def Prepare_To_Sense(self):
         self.sensors = {}
         for linkName in ["BackLeg", "FrontLeg", "Torso"]:
@@ -25,4 +27,4 @@ class ROBOT:
         for motor in self.motors.values():
             motor.Set_Value(self, i)
     def Think(self):
-        pass
+        self.nn.Print()
