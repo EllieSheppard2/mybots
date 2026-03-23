38ab1b6312783517cb5d7cfdac628753a5c8a5b8
EllieSheppard2 <elliesheppard213@gmail.com>
2026-03-23 12:42:20 -0400

    a fully connected neural network

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import pyrosim.pyrosim as pyrosim
-
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -28,14 +28,22 @@ def Generate_Body():
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
 
+    sensor_neurons = [0, 1, 2]
+    motor_neurons = [3, 4]
     pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
     pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=1.0)
+    for i in sensor_neurons:
+        for j in motor_neurons:
+            random_weight = random.uniform(-1, 1)
+            pyrosim.Send_Synapse(
+                sourceNeuronName=i,
+                targetNeuronName=j,
+                weight= random_weight
+            )
 
     pyrosim.End()
 
