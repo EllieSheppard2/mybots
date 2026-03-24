806cefa8eb63eb85dcc18aa01024d4a1e9364160
EllieSheppard2 <elliesheppard213@gmail.com>
2026-03-24 15:03:38 -0400

    step 16

--- search.py
+++ search.py
@@ -1,8 +1,9 @@
 import os
 import sys
-
+from hillclimber import HILL_CLIMBER
 
 #would just say "python3", but modules are in conda environment
-for i in range(5):
-    os.system(f"{sys.executable} generate.py")
-    os.system(f"{sys.executable} simulate.py")
+#for i in range(5):
+#    os.system(f"{sys.executable} generate.py")
+#    os.system(f"{sys.executable} simulate.py")
+hc = HILL_CLIMBER()
---
+++ solution.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+
+class SOLUTION():
+    def __init__(self):
+        self.weights = np.random.rand(3, 2)
+
+        print("Random weights: ")
+        print(self.weights)
+
+        sys.exit()
